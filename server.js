d0be6d72a7fd2b6767bf09c622d97a448bef4ba6
Larry Cessna <larrybcessna@gmail.com>
2020-10-09 21:33:56 -0700

    Created get and post requests. All working.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Dependencies
 // =============================================================
+const { response } = require("express");
 var express = require("express");
+var fs = require("fs");
 var path = require("path");
 
 
@@ -14,6 +16,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let notesDB = fs.readFileSync(path.join(__dirname, "/db/db.json"));
+notesDB = JSON.parse(notesDB)
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -21,13 +26,29 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 app.get("/api/notes", function (req, res) {
-    const notesDB = [{"title":"Test Title","text":"Test text"}];
     return res.json(notesDB);
 });
-
+//Post route
+app.post("/api/notes", function (req, res) {
+    const newNote = req.body;
+    console.log(notesDB.length);
+    if (notesDB.length === 0) {
+        newNote.id = 1;
+        notesDB.push(newNote);
+    } else {
+        newNote.id = Math.max.apply(Math, notesDB.map(function (note) { return note.id; })) + 1;
+        notesDB.push(newNote);
+    }
+    
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(notesDB));
+    return res.json(req.body);
+})
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
+
+
+  //post
+  //delete
